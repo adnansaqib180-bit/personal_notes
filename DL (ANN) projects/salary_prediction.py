a716ed2d85bc24ca9149bf6023763b7ef53b8fd1
adnansaqib180-bit <adnansaqib180@gmail.com>
2026-08-31 20:46:21 +0500

    completed project of regression problem

--- DL (ANN) projects/salary_prediction.py
+++ DL (ANN) projects/salary_prediction.py
@@ -1,1 +1,74 @@
-z
+import pandas as pd 
+from keras import Sequential
+from keras.layers import Dense
+import matplotlib.pylab as plt
+from sklearn.metrics import accuracy_score,r2_score
+from sklearn.model_selection import train_test_split
+import warnings
+warnings.filterwarnings("ignore")
+
+df = pd.read_csv('personal_notes/DL (ANN) projects/new_hr.csv')
+print(df.head())
+print(df.columns)
+print(df.isnull().sum())
+print(df.info())
+
+df = df.drop(columns=['Attrition','EmployeeCount','EmployeeNumber','EnvironmentSatisfaction','MaritalStatus','RelationshipSatisfaction'])
+
+df['OverTime'] = df['OverTime'].map({"Yes":1,'No':0})
+df['Over18'] = df['Over18'].map({"Y":1,'N':0})
+df['Gender'] = df['Gender'].map({'Male':1,"Female":0})
+df['BusinessTravel']  =  df['BusinessTravel'].map({"Non-Travel":0,'Travel_Rarely':1,'Travel_Frequently':2})
+df = pd.get_dummies(data=df,columns=['Department','EducationField','JobRole'],drop_first=True,dtype=int)
+
+print(len(df.columns))
+print(df.info())
+
+x = df.drop(columns=['MonthlyIncome'])
+y = df['MonthlyIncome']
+
+X_train, X_test, y_train, y_test = train_test_split( x, y, test_size=0.33, random_state=42)
+
+model = Sequential()
+model.add(Dense(128,activation='relu',input_dim= len(df.columns)))
+model.add(Dense(64,activation='relu'))
+model.add(Dense(16,activation='relu'))
+model.add(Dense(1,activation='linear'))
+
+print(model.summary())
+
+model.compile(optimizer='Adam',loss='mse',metrics=['accuary'])
+
+history = model.fit(X_train,y_train,epochs=3,validation_split = .24)
+
+predictions = model.predict(X_test)
+
+print("Accuracy Score:", accuracy_score(y_test, predictions))
+print("r2 Score:", r2_score(y_test, predictions))
+
+
+plt.figure(figsize=(10, 4))
+plt.subplot(1, 2, 1)
+plt.plot(history.history["loss"], label="Train Loss")
+plt.plot(history.history["val_loss"], label="Validation Loss")
+plt.title("Model Loss")
+plt.ylabel("Loss")
+plt.xlabel("Epoch")
+plt.legend()
+plt.subplot(1, 2, 2)
+plt.plot(history.history["mse"], label="Train Accuracy")
+plt.plot(history.history["val_mse"], label="Validation Accuracy")
+plt.title("Model Accuracy")
+plt.ylabel("Accuracy")
+plt.xlabel("Epoch")
+plt.legend()
+
+plt.tight_layout()
+plt.show()
+
+
+
+
+
+
+
