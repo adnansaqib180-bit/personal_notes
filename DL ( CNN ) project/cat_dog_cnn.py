f89acc8131be7c8c33d85c8df4ddafa33cdb5f0b
adnansaqib180-bit <adnansaqib180@gmail.com>
2026-09-22 20:04:37 +0500

    add CNN model implementation for cat-dog classification

---
+++ DL ( CNN ) project/cat_dog_cnn.py
@@ -0,0 +1,49 @@
+import Tensorflow as tf
+from keras.models import Sequential 
+from keras.layers import Dense,Flatten , Conv2D, MaxPooling2D
+from keras.utils import image_dataset_from_directory as loader 
+# loading the data 
+train_ds =  loader(
+    directory = '/kaggle/input/datasets/melvinpauljacob/cat-dog-dataset/dogscats/images'
+    labels="inferred",
+    label_mode="int",
+    class_names=None,
+    color_mode="rgb",
+    batch_size=32,
+    image_size=(256, 256)
+)
+
+test_ds =  loader(
+    directory = '/kaggle/input/datasets/melvinpauljacob/cat-dog-dataset/dogscats/valid',
+    labels="inferred",
+    label_mode="int",
+    class_names=None,
+    color_mode="rgb",
+    batch_size=32,
+    image_size=(256, 256)
+)
+#normalizinf the data 
+
+def process(image,lable):
+    image = tf.cast(image/256,tf.float32)
+    return image , lable
+train_ds = train_ds.map(process)
+test_ds = test_ds.map(process) 
+
+# creating our CNN 
+model = Sequential()
+#frist convolution layer with max pooling 
+model.add(Conv2D(32, padding = 'valid',kernel_size = (3,3),input_shape = (256,256,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2),strides=2,padding='valid'))
+# secound layer
+model.add(Conv2D(64, padding='valid',activation='relu',kernel_size = (3,3)))
+model.add(MaxPooling2D(pool_size=(2,2),strides=2,padding='valid'))
+# third layer 
+model.add(Conv2D(128, padding='valid',activation='relu',kernel_size = (3,3)))
+model.add(MaxPooling2D(pool_size=(2,2),strides=2,padding='valid'))
+# coverting to 1D so we can feed to fully connected layers 
+model.add(Flatten())
+# adding layers 
+model.add(Dense(128,activation='relu'))
+model.add(Dense(64,activation='relu'))
+model.add(Dense(1,activation='sigmoid'))
